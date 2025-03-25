e4b5eccfebaec23653996e3ae35b0956331b78bd
saqib1218 <saqibiftikhar756@gmail.com>
2025-03-26 04:31:18 +0500

    button size changed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -56,7 +56,7 @@ const Projects = () => {
       <motion.div
         initial="hidden"
         whileInView="visible"
-        viewport={{ once: true, margin: "-100px" }}
+        viewport={{  margin: "-100px",amount:0.3}}
         variants={containerVariants}
         className="projects-container"
       >
